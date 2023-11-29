2cbe53ec385a757c17c370de678c3c4c84ad0498
taeholee <lth1993@naver.com>
2023-11-29 19:28:56 +0900

    feat :  응답시 CommentList를 Entity가 아닌, dto로 응답

--- src/main/java/com/example/newsfeed/dto/MenuResponseDto.java
+++ src/main/java/com/example/newsfeed/dto/MenuResponseDto.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import org.hibernate.annotations.Type;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -16,7 +17,7 @@ public class MenuResponseDto extends CommonResponseDto{
     private String authorNickname;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
-    private List<Comment> commentList;
+    private List<CommentResponseDto> commentList = new ArrayList<>();
 
     public MenuResponseDto(Menu menu) {
         this.title = menu.getTitle();
@@ -24,7 +25,18 @@ public class MenuResponseDto extends CommonResponseDto{
         this.authorNickname = menu.getUser().getNickname();
         this.createdAt = menu.getCreatedAt();
         this.modifiedAt = menu.getModifiedAt();
-        this.commentList = menu.getCommentList();
+//        this.commentList = menu.getCommentList().stream()
+//                .map(CommentResponseDto::new)
+//                .toList();
+
+        // 여기서 NPE 떴는데 이유는 20번째 줄 new ArrayList<>()로 초기화를 안시켜줌.
+        // 그렇게 되면 초기값이 NULL이라서 NPE가 떠버림!
+        // 저기 위에 주석 처리된건 진짜 고수들의 문법, stream을 사용해서 Comment Entity -> CommentResponseDto로 변환시켜줌
+        for (Comment comment : menu.getCommentList()) {
+            CommentResponseDto commentResponseDto = new CommentResponseDto(comment);
+            this.commentList.add(commentResponseDto);
+        }
     }
+
 
 }
